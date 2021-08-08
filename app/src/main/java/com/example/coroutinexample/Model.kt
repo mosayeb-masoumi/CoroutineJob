3eea7ef002f746739ec5a1dd30a08f592fa9edac
mosayeb <mb.masoumi1000@gmail.com>
2021-08-08 10:46:19 +0430

    first commit

---
+++ app/src/main/java/com/example/coroutinexample/Model.kt
@@ -0,0 +1,15 @@
+package com.example.coroutinexample
+
+class Model {
+
+    var name1 = ""
+    var name2 = ""
+
+
+    constructor( name1: String ,  name2: String  ){
+        this.name1 = name1
+        this.name2 = name2
+    }
+
+    constructor()
+}
