d9d4130d150bfed3e72a0aa9cac2e36814929492
mosayeb <mb.masoumi1000@gmail.com>
2022-02-06 14:03:45 +0330

    master

--- app/src/main/java/com/example/coroutinexample/MainActivity.kt
+++ app/src/main/java/com/example/coroutinexample/MainActivity.kt
@@ -1,149 +1,130 @@
 package com.example.coroutinexample
 
-import android.graphics.Insets.add
+import android.content.Intent
+
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import android.widget.Toast
+
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.coroutines.*
 import kotlinx.coroutines.Dispatchers.IO
 import kotlinx.coroutines.Dispatchers.Main
+import java.io.IOException
 
 class MainActivity : AppCompatActivity() {
 
-    private var count = 1
-
     private lateinit var job: Job
+    var count = 1
+
+    private lateinit var jobParallel1: Job
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+
+        btn_scopes.setOnClickListener {
+            startActivity(Intent(this@MainActivity , ScopeActivity::class.java ))
+        }
+
+
         btn_start.setOnClickListener {
 
+            if (::job.isInitialized) {   // to prevent multiple start
+                if (job.isActive)
+                    job.cancel()
+
+                count = 0
+                CoroutineScope(IO).launch {
+                    showInText(count)
+                }
+            }
             startCounting()
         }
 
 
-        btn_cancel.setOnClickListener {
 
-            if(::job.isInitialized)
+        btn_cancel.setOnClickListener {
+            if (::job.isInitialized)
                 job.cancel()
 
             count = 0
-
-            CoroutineScope(IO).launch{
-                showCounter("0")
+            CoroutineScope(IO).launch {
+                showInText(count)
             }
+
+        }
+
+
+        btn_serie_tasks.setOnClickListener {
+            doSerieTasks()
         }
 
 
 
-        val list1  = arrayListOf<String>().apply {
-            add("reza")
-            add("hassan")
+
+        btn_parallel_tasks.setOnClickListener {
+            doParallelTasks()
         }
-
-        val list2  = arrayListOf<String>().also {
-            it.add("milad")
-            it.add("gholi")
-        }
-
-        list1?.run {
-
-            val name1 = list1[0]
-            val name2 = list1[1]
-
-            val names = name1+name2
-        }
-
-        val obj = Model().apply {
-            name1 = "naghi"
-            name2 = "taghi"
-        }
-        with(obj){
-            name1 = "naghi1"
-            name2 = "taghi2"
-        }
-
-        Log.i("TAG", "onCreate: ")
-
-
-        var result = Model().let {
-            it.name1 = "naghi"
-            it.name2 = "taghi"
-
-            "return this result"
-        }
-
-        var ddd = result
-        var oo = ddd
-
-
-        obj?.run {
-            val name1 = obj.name1
-            val name2 = obj.name2
-
-            Log.i("TAG", "onCreate: ")
-        }
-
-
-
-        val list3 = arrayListOf<Model>().apply {
-            add(Model("reza","rezai"))
-            add(Model("ali","mardani"))
-        }
-
-        list3?.run {
-
-            var name1 = list3[0].name1 + list3[0].name2
-            var name2 = list3[1].name1 + list3[1].name2
-
-            var ss = ""
-        }
-
-
-        val list4 = arrayListOf<String>().apply {
-            add(list3[0].name1 + list3[0].name2)
-            add(list3[1].name1 + list3[1].name2)
-        }.also {
-            var name1 = it[0]
-            Log.i("TAG", "onCreate: ")
-        }
-
-
-
-        list4.run {
-            var name1 = list4[0]
-            var name2 = list4[1]
-
-            var ss = ""
-        }
-        var ss = ""
-
-
 
 
 
     }
+
+    private fun doParallelTasks() {
+
+//        jobParallel1 = Job()   // Job() (if one child failed the other child will failed too)
+        jobParallel1 = SupervisorJob()   // superiorJob() (if one child failed the other child will do its job)
+        val handler = CoroutineExceptionHandler { _, _ -> }
+        val scope = CoroutineScope(IO + jobParallel1 + handler)
+
+        //child1
+        scope.launch {
+            val result1 = withContext(IO) { fetchFirstData() }
+            showInText(result1)
+        }
+
+        //child2
+        scope.launch {
+            val result2 = withContext(IO) { fetchSecondData() }
+            showInText(result2)
+        }
+
+
+    }
+
+
+    private fun doSerieTasks() {
+        CoroutineScope(IO).launch {
+
+            val result1 = withContext(IO) { fetchFirstData() }
+            val result2 = withContext(IO) { fetchSecondData() }
+
+            val total = result1 + result2
+            var tt = total
+
+        }
+    }
+
+
+    private suspend fun fetchFirstData(): Int {
+        delay(4000)
+        throw IOException()  // to create exception  and failed the return
+        return 1
+    }
+
+    private suspend fun fetchSecondData(): Int {
+        delay(8000)
+        return 2
+    }
+
 
     private fun startCounting() {
+
         job = Job()
         CoroutineScope(IO + job).launch {
-            var counter = withContext(IO){getCount()}
-
-            showCounter(counter.toString())
-
-        }
-
-
-    }
-
-    private suspend fun showCounter(count: String) {
-
-        withContext(Main){
-            txt.text = count
+            val count = withContext(IO) { getCount() }
+            showInText(count)
         }
 
     }
@@ -154,19 +135,17 @@ class MainActivity : AppCompatActivity() {
         return count++
     }
 
+    private suspend fun showInText(count: Int) {
 
-    private fun getFun1(): String {
-        return "result1"
-    }
-    private fun getFun2(): String {
-        return "result2"
+        withContext(Main) {
+            txt.text = count.toString()
+        }
     }
 
 
     override fun onDestroy() {
         super.onDestroy()
-
-        if(job.isActive)
-        job.cancel()
+        if (job.isActive)
+            job.cancel()
     }
 }
---
+++ app/src/main/java/com/example/coroutinexample/ScopeActivity.kt
@@ -0,0 +1,96 @@
+package com.example.coroutinexample
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+
+class ScopeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_scope)
+
+
+
+        val list1  = arrayListOf<String>().apply {
+            add("reza")
+            add("hassan")
+        }
+
+        val list2  = arrayListOf<String>().also {
+            it.add("milad")
+            it.add("gholi")
+        }
+
+        list1?.run {
+
+            val name1 = list1[0]
+            val name2 = list1[1]
+
+            val names = name1+name2
+        }
+
+        val obj = Model().apply {
+            name1 = "naghi"
+            name2 = "taghi"
+        }
+        with(obj){
+            name1 = "naghi1"
+            name2 = "taghi2"
+        }
+
+        Log.i("TAG", "onCreate: ")
+
+
+        var result = Model().let {
+            it.name1 = "naghi"
+            it.name2 = "taghi"
+
+            "return this result"
+        }
+
+        var ddd = result
+        var oo = ddd
+
+
+        obj?.run {
+            val name1 = obj.name1
+            val name2 = obj.name2
+
+            Log.i("TAG", "onCreate: ")
+        }
+
+
+
+        val list3 = arrayListOf<Model>().apply {
+            add(Model("reza","rezai"))
+            add(Model("ali","mardani"))
+        }
+
+        list3?.run {
+
+            var name1 = list3[0].name1 + list3[0].name2
+            var name2 = list3[1].name1 + list3[1].name2
+
+            var ss = ""
+        }
+
+
+        val list4 = arrayListOf<String>().apply {
+            add(list3[0].name1 + list3[0].name2)
+            add(list3[1].name1 + list3[1].name2)
+        }.also {
+            var name1 = it[0]
+            Log.i("TAG", "onCreate: ")
+        }
+
+
+        list4.run {
+            var name1 = list4[0]
+            var name2 = list4[1]
+
+            var ss = ""
+        }
+        var ss = ""
+
+    }
+}
